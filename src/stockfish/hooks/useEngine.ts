c7e9efe412f2f350e5235522015350c9f9bca5f1
jalpp <jalp.panchal44@gmail.com>
2025-05-22 21:48:55 -0400

    fix(fish17): fix 17

--- src/stockfish/hooks/useEngine.ts
+++ src/stockfish/hooks/useEngine.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { EngineName } from '../engine/engine';
 import { Stockfish11 } from '../engine/Stockfish11';
-import { Stockfish16 } from '../engine/Stockfish16';
-import { Stockfish17 } from '../engine/Stockfish17';
+// import { Stockfish16 } from '../engine/Stockfish16';
+// import { Stockfish17 } from '../engine/Stockfish17';
 import { UciEngine } from '../engine/UciEngine';
 
 export const useEngine = (enabled: boolean, engineName: EngineName | undefined) => {
@@ -30,9 +30,9 @@ export const useEngine = (enabled: boolean, engineName: EngineName | undefined) 
 const pickEngine = (engine: EngineName): UciEngine => {
     switch (engine) {
         case EngineName.Stockfish17:
-            return new Stockfish17();
+            return new Stockfish11();
         case EngineName.Stockfish16:
-            return new Stockfish16();
+            return new Stockfish11();
         case EngineName.Stockfish11:
             return new Stockfish11();
     }
